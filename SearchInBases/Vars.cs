6f538333fb2d63c68d425c4f9469ccaed466f49d
Giovani da Silva <giovani.silva@paytrack.com.br>
2022-05-01 17:25:16 -0300

    alterado versão

--- SearchInBases/Vars.cs
+++ SearchInBases/Vars.cs
@@ -11,7 +11,7 @@ namespace SearchInBases
     {    
         //App
         public static string appName = "SearchInBases";        
-        public static string appVersion = "1.0.12";
+        public static string appVersion = "1.0.13";
         public static string appNameWithVersion = Vars.appName + "  v." + Vars.appVersion;
         public static string pathApp = AppDomain.CurrentDomain.BaseDirectory;
 
